39625fe8d7009655cfb0f5deeb1fc082531213dd
Gubo Diba <gubodibawario@gmail.com>
2023-06-13 07:26:19 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,234 @@
+import random
+
+name = input("Type your name: ")
+print("Welcome", name, "to our Cement Store!")
+
+answer = input("What Cement do you want to order? Ordinary portland cement/Rapid hardening cement/Sulphate resistance cement/ White cement/Masonary cement: ")
+if answer == "Ordinary portland cement":
+    answer = input("Great!  How many bags of cement do you want to order? Type the number of bags you want? (1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/17/18/19/20):  ") 
+    
+    if answer == "1":
+       print("Your order will be delivered shortly. Thank you for shopping with us!:")
+    elif answer == "2":
+       print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+    elif answer == "3":
+       print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+    elif answer == "4":
+       print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+    elif answer == "5":
+       print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "6":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "7":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "8":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "9":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "10":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "11":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "12":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "13":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "14":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "15":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "16":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "17":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "18":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "19":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "20":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    else:
+      print("Choose another option, Rapid hardening cement/Sulphate resistance cement/ White cement/Masonary cement")
+elif answer == "Rapid hardening cement":
+   answer = input("Great!  How many bags of cement do you want to order? Type the number of bags you want? (1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/17/18/19/20):  ") 
+    
+   if answer == "1":
+       print("Your order will be delivered shortly. Thank you for shopping with us!:")
+   elif answer == "2":
+       print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+   elif answer == "3":
+       print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+   elif answer == "4":
+       print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+   elif answer == "5":
+       print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   elif answer == "6":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   elif answer == "7":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   elif answer == "8":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   elif answer == "9":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   elif answer == "10":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   elif answer == "11":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   elif answer == "12":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   elif answer == "13":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   elif answer == "14":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   elif answer == "15":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   elif answer == "16":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   elif answer == "17":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   elif answer == "18":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   elif answer == "19":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   elif answer == "20":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+   else:
+      print("Choose another option, Ordinary portland cement/Sulphate resistance cement/ White cement/Masonary cement")
+elif answer == "Sulphate resistance cement":
+    answer = input("Great!  How many bags of cement do you want to order? Type the number of bags you want? (1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/17/18/19/20):  ") 
+    if answer == "1":
+       print("Your order will be delivered shortly. Thank you for shopping with us!:")
+    elif answer == "2":
+       print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+    elif answer == "3":
+       print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+    elif answer == "4":
+       print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+    elif answer == "5":
+       print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "6":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "7":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "8":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "9":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "10":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "11":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "12":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "13":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "14":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "15":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "16":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "17":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "18":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "19":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "20":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    else:
+      print("Choose another option, Ordinary portland cement/Sulphate resistance cement/ White cement/Masonary cement")
+          
+elif answer == " White cement":
+    answer = input("Great!  How many bags of cement do you want to order? Type the number of bags you want? (1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/17/18/19/20):  ") 
+    if answer == "1":
+       print("Your order will be delivered shortly. Thank you for shopping with us!:")
+    elif answer == "2":
+       print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+    elif answer == "3":
+       print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+    elif answer == "4":
+       print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+    elif answer == "5":
+       print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "6":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "7":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "8":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "9":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "10":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "11":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "12":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "13":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "14":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "15":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "16":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "17":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "18":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "19":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "20":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    else:
+      print("Choose another option, Ordinary portland cement/Rapid hardening cement/Sulphate resistance cement/Masonary cement")
+elif answer == " Masonary cement":
+    answer = input("Great!  How many bags of cement do you want to order? Type the number of bags you want? (1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/17/18/19/20):  ") 
+    if answer == "1":
+       print("Your order will be delivered shortly. Thank you for shopping with us!:")
+    elif answer == "2":
+       print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+    elif answer == "3":
+       print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+    elif answer == "4":
+       print("Your order will be delivered shortly. Thank you for shopping with us!: ")
+    elif answer == "5":
+       print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "6":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "7":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "8":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "9":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "10":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "11":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "12":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "13":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "14":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "15":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "16":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "17":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "18":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "19":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    elif answer == "20":
+      print("Your order will be delivered shortly, Thank you for shopping with us!: ")
+    else:
+      print("Choose another option, Ordinary portland cement/Rapid hardening cement/Sulphate resistance cement/White cement")
+else:
+      print("Thank you, Shop with us again")
+
+
+
